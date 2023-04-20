b56f2ffb82d7abe99bf84b95ca09914ab3ae72e2
AlexPetrovichJr <you@avidbooklearner@gmail.com>
2023-04-20 00:58:51 -0700

    completed POST PUT and DELETE (Admin) routes for products

--- server/api/products.js
+++ server/api/products.js
@@ -28,4 +28,35 @@ router.get("/:productId", async (req, res, next) => {
   }
 });
 
+// POST /api/products/ (ADMIN)
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+// PUT /api/products/productid (ADMIN)
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.send(await product.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/products/productId
+router.delete('/:produtId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.send(todo);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router
