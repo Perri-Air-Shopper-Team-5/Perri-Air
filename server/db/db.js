b0a1f9c32c3601c88f550caf1958b1ca97572014
AdamTheMaker <adam.bojoh@gmail.com>
2023-04-25 15:51:56 -0400

    adjusted db code to allow for local connections

--- server/db/db.js
+++ server/db/db.js
@@ -1,26 +1,32 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName =
+    pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const config = {
-  logging: false
+    logging: false,
 };
 
-if(process.env.LOGGING === 'true'){
-  delete config.logging
+if (process.env.LOGGING === "true") {
+    delete config.logging;
 }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
+if (process.env.DATABASE_URL) {
+    config.dialectOptions = {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgresql://elucero9:v2_43FJu_g2PHvWYKykLXf8GbKi3W9DE@db.bit.io:5432/elucero9/${databaseName}?ssl=true`, config)
-module.exports = db
-// postgresql://elucero9:v2_43FJu_g2PHvWYKykLXf8GbKi3W9DE@db.bit.io:5432/elucero9/perri_air
+    //local db
+    process.env.DATABASE_URL || `postgresql://localhost:5432/${databaseName}`,
+    //deployment db
+    //     process.env.DATABASE_URL ||
+    //         `postgresql://elucero9:v2_43FJu_g2PHvWYKykLXf8GbKi3W9DE@db.bit.io:5432/elucero9/perri_air?ssl=true`,
+    config
+);
+module.exports = db;
